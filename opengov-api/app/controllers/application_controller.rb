75dbcd469de51fb40ddbeff8560df3cc00d31c30
mmwebster <milo.webster@gmail.com>
2017-07-13 13:21:56 -0700

    Added new structuring

--- opengov-api/app/controllers/application_controller.rb
+++ opengov-api/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  # Prevent CSRF attacks by raising exception
+  protect_from_forgery with: :null_session
 end
--- opengov-api/config/routes.rb
+++ opengov-api/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: "home#index" # not sure if necessary
+
+  namespace :api do
+    namespace :v1, :defaults => { :format = :json } do
+      jsonapi_resources :web_status
+      jsonapi_resources :web_data
+    end
+  end
 end
