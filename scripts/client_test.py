28381c5533502a8b6b7ed6fd702304dfef9997ba
mmwebster <milo.webster@gmail.com>
2017-07-18 12:29:40 -0700

    Separated local api url from remote url in py test client

--- scripts/client_test.py
+++ scripts/client_test.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # (python 2.7)
 
+# Example usage:
+#   python client_test.py https://google.com
+
 import requests
 from time import sleep
 import sys
 
-url = 'http://localhost:5000/api/v1/web-statuses?filter[url]=ahttp://' \
-      'statisticstimes.com/economy/countries-by-projected-gdp2.php'
+base_api_url = 'http://localhost:5000/api/v1/web-statuses?filter[url]='
+remote_url = 'http://statisticstimes.com/economy/countries-by-projected-gdp2.php'
 
 if len(sys.argv) < 2:
     print("WARNING => No URL argument passed")
 else:
-    url = sys.argv[1]
+    remote_url = sys.argv[1]
 
 for i in range(9):
-    response = requests.get(url)
+    response = requests.get(base_api_url + remote_url)
     print("RESPONSE => " + str(response.json()))
     sleep(1)
