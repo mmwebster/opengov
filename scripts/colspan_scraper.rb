36cb8b7f51ffe7f94c9aeaa314c541b26fd39209
David Krieger <frebreezespam@gmail.com>
2017-07-22 16:36:29 -0700

    Integrated Scraper, Daniel will finish hashing

--- scripts/colspan_scraper.rb
+++ scripts/colspan_scraper.rb
@@ -2,7 +2,13 @@ require 'json'
 require 'nokogiri'
 require 'open-uri'
 
+def string_clean(string)
+  string.delete!("\n")
+  string.split.join(" ")
+end
+
 def parse_tables(page_url)
+
 
   if(page_url == nil || page_url == '') then
     puts "Error reading given URL (nil)"
@@ -17,26 +23,28 @@ def parse_tables(page_url)
 
   # For each table on the page...
   page_tables.each do |table|
-    table_headers = []
-    table_data = []
 
-    # A header row is defined as one which has two or more th children and is not empty
+    $potential_bad_table_data = "The table was read in successfully"
+
+    table_headers = []
+
+    # A header row is defined as a row which: is not anywhere inside a
+    # tbody element; contains two or more th children; and is not empty
     table_rows = table.css('tr')
-    num_header_rows = 0
-    num_body_rows = 0
-    
+
     # Calculate the number of header rows at the tanle's top
+    num_header_rows = 0
     table_rows.map do |row|
       if(row.css('th').length > 1) then
         num_header_rows += 1
-      else 
+      else
         break
       end
     end
-    
+
     num_body_rows = table_rows.length - num_header_rows
 
-    puts "==========================================================================================="
+    #puts "==========================================================================================="
     puts
     STDOUT.puts "Begin processing table \##{table_counter.to_s.rjust(2, '0')}"
     STDOUT.puts "Table \##{table_counter.to_s.rjust(2, '0')}: total number of rows: #{table_rows.length}"
@@ -46,8 +54,8 @@ def parse_tables(page_url)
 
 
     $span_check = []
-
-    table_array = [[],[]]
+    $final_span = 0
+    table_header_array = [[],[]]
 
     for i in 0..(num_header_rows - 1)
 
@@ -65,16 +73,17 @@ def parse_tables(page_url)
             #Append current text from current column into all subsequent lower rows
             for j in i ..(num_header_rows - 1)
 
-              if table_array[j][k].to_s == ""
-                table_array[j][k] = "#{check.text}"
+              if table_header_array[j][k].to_s == ""
+                table_header_array[j][k] = "#{check.text}"
               else
-                table_array[j][k] = "#{table_array[j][k].to_s}.#{check.text}"
+                table_header_array[j][k] = "#{table_header_array[j][k].to_s}.#{check.text}"
               end
 
-              puts "Current value of table_array at #{j} #{k}: #{table_array[j][k]}"
+              string_clean(table_header_array[j][k])
+              #puts "Current value of table_header_array at #{j} #{k}: #{table_header_array[j][k]}"
 
             end
-            puts
+            #puts
 
           end
 
@@ -86,14 +95,15 @@ def parse_tables(page_url)
           #first insert current phrase at location and append to all subsequent rows beneath
           for j in i ..(num_header_rows - 1)
 
-            if table_array[j][$span_check[i]].to_s == ""
-              table_array[j][$span_check[i]] = "#{check.text}"
+            if table_header_array[j][$span_check[i]].to_s == ""
+              table_header_array[j][$span_check[i]] = "#{check.text}"
             else
-              table_array[j][$span_check[i]] = "#{table_array[j][$span_check[i]].to_s}.#{check.text}"
+              table_header_array[j][$span_check[i]] = "#{table_header_array[j][$span_check[i]].to_s}.#{check.text}"
             end
 
-            puts "Current value of table_array at #{j} #{$span_check[i]}: #{table_array[j][$span_check[i]]}"
-            puts
+            string_clean(table_header_array[j][$span_check[i]])
+            #puts "Current value of table_header_array at #{j} #{$span_check[i]}: #{table_header_array[j][$span_check[i]]}"
+            #puts
 
           end
 
@@ -103,10 +113,69 @@ def parse_tables(page_url)
 
       end
 
+      #final_span is the # of columns to search through
+      if $final_span < $span_check[i]
+        $final_span = $span_check[i]
+        #puts "Value of final span is now #{$final_span}"
+      end
+
     end
 
-    puts "\n=========================================================================================="
-    puts
+    #puts "\n=========================================================================================="
+    #puts
+
+    table_data = Array.new(num_body_rows){Array.new($final_span)}
+
+    for i in 0..(num_body_rows - 1)
+
+      body_index = i + num_header_rows
+      has_header = table_rows[body_index].css("th")
+      row_data = table_rows[body_index].css("td")
+
+      for j in 0..($final_span - 1)
+
+        col_header = table_header_array[num_header_rows - 1][j]
+
+        if row_data[j] == nil
+          table_data[i][j] =  "#{body_index}, "
+
+        #contains a header on left side, append to each table head of the column
+        elsif has_header[0].to_s != ""
+
+          special_headers = "#{col_header}.#{has_header[0].text}"
+          string_clean(special_headers)
+
+          #Row data != num of columns, null spaces make it hard to align data
+          if row_data.length != ($final_span - 1)
+            $potential_bad_table_data = "The table was irregular and could contain incorrect data"
+          end
+
+          #Store all table data now
+          if j != 0
+            table_data[i][j] = "#{body_index}, #{row_data[j-1].text}"
+            string_clean(table_data[i][j])
+            puts "Header type: Index is #{i} #{j}\nStored the row, text:  #{table_data[i][j]} \ndatatype of #{special_headers}"
+          end
+
+        #no head found, just do it normally
+        else
+
+          #Row data != num of columns, null spaces make it hard to align data
+          if row_data.length != $final_span
+            $potential_bad_table_data = "The table was irregular and could contain incorrect data"
+          end
+
+          table_data[i][j] =  "#{body_index}, #{row_data[j].text}"
+          string_clean(table_data[i][j])
+
+          puts "Stored in format 'row, value': #{table_data[i][j]}"
+          puts "Is of datatype: #{col_header}"
+
+
+        end
+        puts
+      end
+    end
 
     # If valid headers exist, then for each one...
     for i in 0...(table_rows.length - 1)
@@ -129,6 +198,10 @@ def parse_tables(page_url)
     })
 
     table_counter += 1
+    puts "\n==============================="
+    puts "#{$potential_bad_table_data}"
+    puts "================================"
+
   end
 
   return table_hashes
@@ -137,11 +210,16 @@ end
 
 if __FILE__ == $0 then
 
-  puts '\nBegin example scraping of colspan based charts via epa'
-  puts 'https://www.epa.gov/recalls/fuel-economy-label-updates#ford'
+  #puts '\nBegin example scraping of colspan based charts via epa'
+  #puts 'https://www.epa.gov/recalls/fuel-economy-label-updates#ford'
   puts
 
-  tables = parse_tables('https://www.epa.gov/recalls/fuel-economy-label-updates#ford')
+  tables = parse_tables('https://www.cia.gov/library/publications/the-world-factbook/rankorder/2127rank.html')
+  #tables = parse_tables('https://www.cdc.gov/asthma/most_recent_data.htm')
+  #tables = parse_tables('https://www.epa.gov/recalls/fuel-economy-label-updates#ford')
+
+
+=begin
   tables.each do |t|
     json = t.to_json
     if(json.length > 1024) then puts json[0...1024] + '...'
@@ -150,7 +228,7 @@ if __FILE__ == $0 then
     puts
   end
 
-=begin
+
   puts 'Begin example scraping of CDC data: most recent asthma data'
   puts 'https://www.cdc.gov/asthma/most_recent_data.htm'
   puts
@@ -173,6 +251,9 @@ if __FILE__ == $0 then
     end
     puts
   end
+
+=end
+
   # Some work needed: header/body row detection causes a crash
   #
   # puts 'Begin example scraping of FBI data: Homicides by Weapon Type'
@@ -181,9 +262,9 @@ if __FILE__ == $0 then
   #      'expanded_homicide_data_table_8_murder_victims_by_weapon_2009-2013.xls'
   # puts
   #
-  # tables = parse_tables('https://ucr.fbi.gov/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/'\
-  #     'offenses-known-to-law-enforcement/expanded-homicide/'\
-  #     'expanded_homicide_data_table_8_murder_victims_by_weapon_2009-2013.xls')
+  #tables = parse_tables('https://ucr.fbi.gov/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/'\
+  #    'offenses-known-to-law-enforcement/expanded-homicide/'\
+  #    'expanded_homicide_data_table_8_murder_victims_by_weapon_2009-2013.xls')
   # tables.each do |t|
   #   json = t.to_json
   #   if(json.length > 1024) then puts json[0...1024] + '...'
@@ -191,7 +272,6 @@ if __FILE__ == $0 then
   #   end
   #   puts
   # end
-=end
 
 
 
