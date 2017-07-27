aec40d287b475b6563ce679989a4c2f1c734bca1
mmwebster <milo.webster@gmail.com>
2017-07-27 14:55:25 -0700

    Made client test less error prone

--- scripts/client_test.py
+++ scripts/client_test.py
@@ -24,6 +24,9 @@ web_page_remote_url = "https://www.cia.gov/library/publications/t" + \
 # user_query_str = "SELECT country FROM '" + \
 #                   web_page_remote_url + \
 #                  "' WHERE date-of-information != 2016"
+# user_query_str = "SELECT country FROM '" + \
+#                   web_page_remote_url + \
+#                  "' WHERE rank < 10"
 user_query_str = "SELECT country FROM '" + \
                   web_page_remote_url + \
                  "' WHERE rank < 10"
@@ -37,33 +40,35 @@ query_completed = False
 
 while not query_completed:
     # initiate scaping/parsing and/or check status
-    response_data = requests.get( base_api_url +
-                                  web_status_endpoint +
-                                  web_page_remote_url ).json()['data']
-    if response_data:
-        web_status_record = response_data[0]
-        page_is_parsed = web_status_record['attributes']['is-parsed']
-        if page_is_parsed:
-            print("SUCCESS: The page was successfully parsed. Now " + \
-                  "querying its data.")
-            # query data
-            # encode for url param
-            encoded_query_str = urlencode({ 'filter[query]': user_query_str })
-            print("STATUS: Encoded query to => " + str(encoded_query_str))
-            # make the GET request and store response
-            response = requests.get( base_api_url +
-                                     web_data_endpoint +
-                                     encoded_query_str )
-            # print out the JSON-API-spec'd response in a readable format
-            if response.json()["data"]:
-                print("STATUS: The following data matched this query.\n")
-                for datum in response.json()["data"]:
-                    print("\nDatum #" + datum["id"])
-                    for key, value in datum["attributes"].iteritems():
-                        print("      " + str(key) + " => " + str(value))
+    response = requests.get( base_api_url +
+                             web_status_endpoint +
+                             web_page_remote_url ).json()
+    if 'data' in response.keys():
+        response_data = response['data']
+        if response_data:
+            web_status_record = response_data[0]
+            page_is_parsed = web_status_record['attributes']['is-parsed']
+            if page_is_parsed:
+                print("SUCCESS: The page was successfully parsed. Now " + \
+                      "querying its data.")
+                # query data
+                # encode for url param
+                encoded_query_str = urlencode({ 'filter[query]': user_query_str })
+                print("STATUS: Encoded query to => " + str(encoded_query_str))
+                # make the GET request and store response
+                response = requests.get( base_api_url +
+                                         web_data_endpoint +
+                                         encoded_query_str )
+                # print out the JSON-API-spec'd response in a readable format
+                if 'data' in response.json().keys():
+                    print("STATUS: The following data matched this query.\n")
+                    for datum in response.json()["data"]:
+                        print("\nDatum #" + datum["id"])
+                        for key, value in datum["attributes"].iteritems():
+                            print("      " + str(key) + " => " + str(value))
+                else:
+                    print("STATUS: No data matched this query.\n")
+                query_completed = True
             else:
-                print("STATUS: No data matched this query.\n")
-            query_completed = True
-        else:
-            print("STATUS: Parsing...")
-            sleep(4)
+                print("STATUS: Parsing...")
+                sleep(4)
